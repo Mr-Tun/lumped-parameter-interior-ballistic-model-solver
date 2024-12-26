c770ffaaada05f58bdef05786761b0dde1167aa8
Yufan CHEN <115021366+Mr-Tun@users.noreply.github.com>
2024-12-26 19:08:13 +0800

    Update run_rk4.m

--- run_rk4.m
+++ run_rk4.m
@@ -6,12 +6,11 @@ function [t, y] = run_rk4(f, t0, y0, h, tf)
     y(:, 1) = y0;
 
     for i = 1:n-1
-        k1 = h * f(t(i), y(:, i));
-        k2 = h * f(t(i) + h/2, y(:, i) + k1/2);
-        k3 = h * f(t(i) + h/2, y(:, i) + k2/2);
-        k4 = h * f(t(i) + h, y(:, i) + k3);
+        k1 = h * f(y(:, i));
+        k2 = h * f(y(:, i) + k1/2);
+        k3 = h * f(y(:, i) + k2/2);
+        k4 = h * f(y(:, i) + k3);
 
         y(:, i+1) = y(:, i) + (k1 + 2*k2 + 2*k3 + k4)/6;
     end
 end
-
